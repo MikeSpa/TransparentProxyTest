ceead4c0e329647293251c3f08c71b7a3473b833
Spahr Michael <spahr.mike@protonmail.com>
2022-01-19 18:59:38 +0100

    deploy TransparantUpgreadableProxy and interact with box through the proxy

--- README.md
+++ README.md
@@ -5,6 +5,4 @@ Test the Transparent Proxy pattern for upgrading smart contracts.
 
 1. Deploy a `Box` implementation contract
 2. Deploy a `ProxyAdmin` contract to be the admin of the proxy
-
-
-
+3. Deploy a `TransparentUpgradeableProxy` to be the proxy for the implementations
--- scripts/01_deploy_box.py
+++ scripts/01_deploy_box.py
@@ -21,3 +21,26 @@ def main():
     proxy_admin = ProxyAdmin.deploy(
         {"from": account},
     )
+
+    # If we want an intializer function we can add
+    # `initializer=box.store, 1`
+    # to simulate the initializer being the `store` function
+    # with a `newValue` of 1
+    box_encoded_initializer_function = encode_function_data()
+    # box_encoded_initializer_function = encode_function_data(initializer=box.store, 1)
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,  # or account address
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1_000_000},  # proxies often need the gas limit
+    )
+    print(f"Proxy deployed to {proxy} ! You can now upgrade it to BoxV2!")
+    # usually we would do box.retrieve()
+    # but now we want to call these function on the proxy
+    # since that address will not change unlike the logic contract address
+    # We assign the abi of box contract to proxy_box
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    print(f"Here is the initial value in the Box: {proxy_box.retrieve()}")
+    store_tx = proxy_box.store(8, {"from": account})
+    store_tx.wait(1)
+    print(f"The value in the Boxis now: {proxy_box.retrieve()}")
